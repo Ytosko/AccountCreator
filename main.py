aef224e95ec5272b25ece70cc4f63ddc297c32ac
Saiki Sarkar <fahim91@student.sust.edu>
2024-08-21 04:27:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import pandas as pd
-from firebase_config import initialize_firebase, create_user
+import firebase_admin
+from firebase_admin import credentials, auth
 
-# Initialize Firebase
-initialize_firebase()
+# Global variable to keep track of Firebase app initialization
+default_app_initialized = False
+
+def initialize_firebase():
+    global default_app_initialized
+    if not default_app_initialized:
+        # Initialize Firebase only if it's not already initialized
+        cred = credentials.Certificate(st.secrets["firebase"]['service '])
+        firebase_admin.initialize_app(cred)
+        default_app_initialized = True
+
+def create_user(email, password):
+    return auth.create_user(email=email, password=password)
+
 
 def add_single_user(email, password):
     try:
